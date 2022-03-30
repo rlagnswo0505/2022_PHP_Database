5d190b9c441f7c1159097ad698fe6700b4d7f419
HoonJae <50700722+rlagnswo0505@users.noreply.github.com>
2022-03-30 12:05:13 +0900

    Create day_3.sql

---
+++ day_3.sql
@@ -0,0 +1,61 @@
+-- dept_emp 테이블에서 dept_no 종류가 몇개가 있는지?
+-- distinct 중복된 것은 하나만 보여주기
+SELECT DISTINCT dept_no FROM dept_emp;
+
+-- 사원의 직무가 무엇이 있는지 select 해주시오.
+
+SELECT DISTINCT title 
+FROM titles 
+ORDER BY title;
+
+-- Limit ? ////////////// Limit ?, ?
+-- 값을 한개 적었을때는 처음부터 개수
+SELECT * FROM employees
+LIMIT 30;
+
+-- 값을 2개 적었을때는 첫번째 값 부터 두번째 값 개수
+SELECT * FROM employees
+LIMIT 0, 10;
+
+/* 가장 많은 연봉을 반은 금액 3개만 나오도록 해주세요 */
+SELECT * FROM salaries
+ORDER BY salary DESC
+LIMIT 3;
+
+/* 연봉 5~10위만 나오도록 */
+SELECT * FROM salaries
+ORDER BY salary DESC
+LIMIT 4, 6;
+
+-- 테이블 컬럼, 데이터타입, 레코드 복사
+CREATE TABLE departments_temp2 AS
+SELECT * FROM departments;
+
+-- 테이블 스키마 복사 (레코드 X)
+CREATE TABLE departments_temp3
+LIKE departments;
+
+-- 데이터를 넣어줌
+INSERT INTO departments_temp3
+SELECT * FROM departments;
+
+-- 그룹 함수 min, max, sum, avg, count
+
+-- group by 를 사용 안 했을 때는 전체에서 값을 가져온다.
+SELECT MIN(salary),MAX(salary), SUM(salary), AVG(salary), COUNT(emp_no) FROM salaries;
+
+
+SELECT emp_no, MIN(salary),MAX(salary), SUM(salary), AVG(salary),COUNT(*)
+FROM salaries
+GROUP BY emp_no;
+
+/* 부서별 사원 수*/
+
+SELECT title, COUNT(*)
+FROM titles
+GROUP BY title;
+
+
+
+
+
