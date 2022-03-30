cba5b326525a8a37ec938d1dd989330948829772
HoonJae <50700722+rlagnswo0505@users.noreply.github.com>
2022-03-30 13:16:41 +0900

    Update day_3.sql

--- day_3.sql
+++ day_3.sql
@@ -62,4 +62,66 @@ GROUP BY dept_no
 HAVING COUNT(*) >= 20000
 ORDER BY COUNT(*) DESC;
 
+/* 가장 적은 salary를 받은 사람의 이름이 나오도록 해주세요*/
+SELECT * FROM employees
+WHERE emp_no =
+(
+SELECT emp_no FROM salaries
+WHERE salary = (SELECT MIN(salary) FROM salaries)
+);
 
+SELECT emp_no FROM salaries
+ORDER BY salary LIMIT 1;
+
+-- auto_increment
+USE `test`;
+
+CREATE TABLE t_hobbit(
+i_hobbit INT UNSIGNED AUTO_INCREMENT,
+nm VARCHAR(10) UNIQUE NOT NULL,
+PRIMARY KEY (i_hobbit)
+);
+
+INSERT INTO t_hobbit (nm)
+VALUES ('농구');
+
+INSERT INTO t_hobbit (nm)
+VALUES ('축구');
+
+INSERT INTO t_hobbit (nm)
+VALUES ('배구');
+
+SELECT * FROM t_hobbit;
+
+CREATE TABLE t_hobbit_2 (
+hobbit_id INT UNSIGNED PRIMARY KEY,
+NAME VARCHAR(20) NOT NULL,
+create_at DATETIME DEFAULT NOW()
+);
+
+INSERT INTO t_hobbit_2 
+(hobbit_id, NAME)
+VALUES
+(1, '핸드볼');
+
+INSERT INTO t_hobbit_2 
+(hobbit_id, NAME, create_at)
+VALUES
+(2, '핸드볼', '2022-02-10');
+
+UPDATE t_hobbit_2
+set NAME = '등산'
+WHERE hobbit_id = 2;
+
+
+INSERT INTO t_hobbit_2 
+(hobbit_id, NAME, create_at)
+VALUES
+(3, '독서', '2022-02-10 12:12:13');
+
+SELECT * FROM t_hobbit;
+SELECT * FROM t_hobbit_2;
+
+INSERT INTO t_hobbit_2
+(hobbit_id, NAME)
+SELECT i_hobbit + 3, nm FROM t_hobbit;
